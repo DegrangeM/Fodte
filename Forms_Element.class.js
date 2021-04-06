8cbc306bf5ec042b9e017fc86c380b9ed7428fcd
DegrangeM <53106394+DegrangeM@users.noreply.github.com>
2021-04-06 10:55:41 +0200

    Changement syntaxe définition classes

--- Forms_Element.class.js
+++ Forms_Element.class.js
@@ -23,52 +23,50 @@ class Form_Element {
     }
 }
 
-Forms = {
-    "form:text": class extends Form_Element {
-        static getValue(e) {
-            return e.getAttribute('form:current-value') || '';
-        }
-    },
-    "form:textarea": class extends Form_Element {
-        static getValue(e) {
-            return e.getAttribute('form:current-value') || '';
-        }
-    },
-    "form:checkbox": class extends Form_Element {
-        static getName(e) {
-            return e.getAttribute('form:label') || e.getAttribute('form:name');
-        }
-        static getValue(e) {
-            return e.getAttribute('form:current-state') == 'checked' ? e.getAttribute('form:value') || '[x]' : e.getAttribute('form:current-state') == 'unknow' ? '[?]' : '[_]';
-        }
-    },
-    "form:radio": class extends Form_Element {
-        static getName(e) {
-            return e.getAttribute('form:name') || e.getAttribute('formx:group-name');
-        }
-        static getValue(e) {
-            // Autre méthode : doc.querySelectorAll('radio[*|group-name="toto"][*|current-selected]');
-            const attribute = e.getAttribute('formx:group-name') ? 'formx:group-name' : 'form:name';
+Forms = {}
+Forms["form:text"] = class extends Form_Element {
+    static getValue(e) {
+        return e.getAttribute('form:current-value') || '';
+    }
+};
+Forms["form:textarea"] = class extends Form_Element {
+    static getValue(e) {
+        return e.getAttribute('form:current-value') || '';
+    }
+};
+Forms["form:checkbox"] = class extends Form_Element {
+    static getName(e) {
+        return e.getAttribute('form:label') || e.getAttribute('form:name');
+    }
+    static getValue(e) {
+        return e.getAttribute('form:current-state') == 'checked' ? e.getAttribute('form:value') || '[x]' : e.getAttribute('form:current-state') == 'unknow' ? '[?]' : '[_]';
+    }
+};
+Forms["form:radio"] = class extends Form_Element {
+    static getName(e) {
+        return e.getAttribute('form:name') || e.getAttribute('formx:group-name');
+    }
+    static getValue(e) {
+        // Autre méthode : doc.querySelectorAll('radio[*|group-name="toto"][*|current-selected]');
+        const attribute = e.getAttribute('formx:group-name') ? 'formx:group-name' : 'form:name';
 
-            return Array.from(e.parentNode.getElementsByTagName('form:radio'))
-                .filter(x => x.getAttribute(attribute) == e.getAttribute(attribute) && x.getAttribute('form:current-selected') == 'true')
-                .map(x => x.getAttribute('form:value') || x.getAttribute('form:label') || e.getAttribute('form:name'))
-                .join(',');
-        }
-        static ignore(e) {
-            const attribute = e.getAttribute('formx:group-name') ? 'formx:group-name' : 'form:name';
+        return Array.from(e.parentNode.getElementsByTagName('form:radio'))
+            .filter(x => x.getAttribute(attribute) == e.getAttribute(attribute) && x.getAttribute('form:current-selected') == 'true')
+            .map(x => x.getAttribute('form:value') || x.getAttribute('form:label') || e.getAttribute('form:name'))
+            .join(',');
+    }
+    static ignore(e) {
+        const attribute = e.getAttribute('formx:group-name') ? 'formx:group-name' : 'form:name';
 
-            return headers.ids.map(x => e.parentNode.querySelector('form>*[*|id="' + x + '"]')).filter(x => x && x.getAttribute(attribute) == e.getAttribute(attribute)).length > 0;
-        }
-    },
-    "form:listbox": class extends Form_Element {
-        static getValue(e) {
-            // Autre méthode : Array.prototype.map.call(document.getElementsByTagName('div'), x=>x.innerHTML)
-            return Array.from(e.getElementsByTagName('form:option'))
-                .filter(x => x.getAttribute('form:current-selected') == 'true')
-                .map(x => x.getAttribute('form:value') || x.getAttribute('form:label'))
-                .join(';');
-        }
-    },
-
-}
+        return headers.ids.map(x => e.parentNode.querySelector('form>*[*|id="' + x + '"]')).filter(x => x && x.getAttribute(attribute) == e.getAttribute(attribute)).length > 0;
+    }
+};
+Forms["form:listbox"] = class extends Form_Element {
+    static getValue(e) {
+        // Autre méthode : Array.prototype.map.call(document.getElementsByTagName('div'), x=>x.innerHTML)
+        return Array.from(e.getElementsByTagName('form:option'))
+            .filter(x => x.getAttribute('form:current-selected') == 'true')
+            .map(x => x.getAttribute('form:value') || x.getAttribute('form:label'))
+            .join(';');
+    }
+};
